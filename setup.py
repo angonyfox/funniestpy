8bb514f78aced5a4613ede576708702503a6f8c5
imptum <imptum@gmail.com>
2018-06-03 13:06:16 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='funniest',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/angonyfox/funniestpy',
+      author='Flying Circus',
+      author_email='angonyfox@gmail.com',
+      license='MIT',
+      packages=['funniest'],
+      zip_safe=False)
